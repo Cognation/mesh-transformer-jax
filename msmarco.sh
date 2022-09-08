053fe9b5de8bdbc437d3ad4f306df51eed4c0e04
Abhigyan Shanker <abhigyanshanker7@gmail.com>
2022-09-08 21:27:12 +0530

    ms-marco-run script

--- msmarco.sh
+++ msmarco.sh
@@ -1,128 +1,128 @@
- python3 ms-marco-jax/file01.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file01.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file02.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file02.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file03.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file03.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file04.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file04.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file05.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file05.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file06.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file06.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file07.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file07.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file08.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file08.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file09.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file09.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file10.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file10.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file11.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file11.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file12.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file12.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file13.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file13.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file14.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file14.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file15.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file15.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file16.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file16.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file17.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file17.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file18.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file18.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file19.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file19.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file20.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file20.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file21.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file21.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file22.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file22.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file23.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file23.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file24.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file24.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file25.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file25.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file26.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file26.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file27.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file27.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file28.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file28.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file29.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file29.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file30.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file30.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file31.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file31.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
- python3 ms-marco-jax/file32.txt marco --output-dir ./marco-tf/ --verbose
- gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
- rm -rf marco-tf/*.tfrecords
+time ms-marco-jax/file32.txt marco --output-dir ./marco-tf/ --verbose
+time gcloud alpha storage cp marco-tf/*.tfrecords gs://gptjax_data/ms-marco-tfrecords/
+time rm -rf marco-tf/*.tfrecords
 
