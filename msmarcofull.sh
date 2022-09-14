553d751ada7d445e93e8963a482f21dd8046dac5
Abhigyan Shanker <abhigyanshanker7@gmail.com>
2022-09-14 16:28:39 +0530

    added msmarcofull run script

---
+++ msmarcofull.sh
@@ -0,0 +1,63 @@
+time python3 create_finetune_tfrecords.py ms-marco-jax/file01.txt marcofull_00000 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file02.txt marcofull_00001 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file03.txt marcofull_00002 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file04.txt marcofull_00003 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file05.txt marcofull_00004 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file06.txt marcofull_00005 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file07.txt marcofull_00006 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file08.txt marcofull_00007 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file09.txt marcofull_00008 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file10.txt marcofull_00009 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file11.txt marcofull_00010 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file12.txt marcofull_00011 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file13.txt marcofull_00012 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file14.txt marcofull_00013 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file15.txt marcofull_00014 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
+
+time python3 create_finetune_tfrecords.py ms-marco-jax/file16.txt marcofull_00015 --output-dir ./marco-tf/ --verbose
+time gsutil cp marco-tf/*.tfrecords gs://gptjax_data/msmarco_full/
+time rm -rf marco-tf/*.tfrecords
